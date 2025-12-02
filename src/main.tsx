1c8625fe4a57186dbb04f59425ddfe69b38eab1a
Vadim Firulyov <firulvv@mail.ru>
2025-12-03 01:01:14 +0400

    ♻️ (main.tsx): закомментирован код для локального режима работы с Telegram WebApp 💡 (main.tsx): добавлены комментарии для улучшения читаемости Код, отвечающий за локальный режим работы с Telegram WebApp, закомментирован для предотвращения его выполнения. Это позволяет избежать ошибок при запуске приложения в среде, где Telegram WebApp не доступен. Комментарии добавлены для улучшения понимания кода и его назначения.

--- src/main.tsx
+++ src/main.tsx
@@ -2,31 +2,31 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-if (!window.Telegram) {
-  // локальный режим
-  window.Telegram = {
-    WebApp: {
-      initData: "",
-      initDataUnsafe: {},
-      expand: () => console.log("[MOCK] expand()"),
-      ready: () => console.log("[MOCK] ready()"),
-      colorScheme: "light",
-      // @ts-ignore
-      MainButton: {
-        text: "",
-        isVisible: false,
-        show: () => console.log("[MOCK] MainButton.show()"),
-        hide: () => console.log("[MOCK] MainButton.hide()"),
-        setText: (t: string) => console.log("[MOCK] setText:", t),
-        showProgress: () => console.log("[MOCK] MainButton.progress"),
-        hideProgress: () => console.log("[MOCK] hideProgress"),
-        onClick: () => {},
-      },
-      showAlert: (msg: string) => alert("[MOCK] " + msg),
-      showConfirm: (msg: string) => Promise.resolve(confirm("[MOCK] " + msg)),
-    },
-  };
-}
+// if (!window.Telegram) {
+//   // локальный режим
+//   window.Telegram = {
+//     WebApp: {
+//       initData: "",
+//       initDataUnsafe: {},
+//       expand: () => console.log("[MOCK] expand()"),
+//       ready: () => console.log("[MOCK] ready()"),
+//       colorScheme: "light",
+//       // @ts-ignore
+//       MainButton: {
+//         text: "",
+//         isVisible: false,
+//         show: () => console.log("[MOCK] MainButton.show()"),
+//         hide: () => console.log("[MOCK] MainButton.hide()"),
+//         setText: (t: string) => console.log("[MOCK] setText:", t),
+//         showProgress: () => console.log("[MOCK] MainButton.progress"),
+//         hideProgress: () => console.log("[MOCK] hideProgress"),
+//         onClick: () => {},
+//       },
+//       showAlert: (msg: string) => alert("[MOCK] " + msg),
+//       showConfirm: (msg: string) => Promise.resolve(confirm("[MOCK] " + msg)),
+//     },
+//   };
+// }
 
 window.Telegram.WebApp.expand();
 window.Telegram.WebApp.ready();
